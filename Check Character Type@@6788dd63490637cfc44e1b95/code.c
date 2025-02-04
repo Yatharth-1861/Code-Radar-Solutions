945c0b77d47fbbab22ad1053de6b23fbc67a9ea8
Yatharth-1861 <yatharth1861.becse24@chitkara.edu.in>
2025-02-05 01:51:13 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -12,6 +12,12 @@ int main() {
         if(lower_ch=='a' || lower_ch == 'e' || lower_ch == 'i' || lower_ch == 'o' || lower_ch =='u'){
             printf("Vowel");
         }
+        else{
+            printf("Consonant");
+        }
+    }
+    else{
+        printf("Special Character");
     }
     return 0;
 }
